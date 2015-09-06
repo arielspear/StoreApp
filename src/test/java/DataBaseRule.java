b6df0ea06576dd4fd54d1823a3dcdc8e10d17aeb
Ariel Spear <simpleredfox@gmail.com>
2015-09-06 12:56:32 -0700

    update DB names in template

--- src/test/java/DataBaseRule.java
+++ src/test/java/DataBaseRule.java
@@ -4,13 +4,13 @@ import org.sql2o.*;
 public class DataBaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/TESTDBNAME", "jake", "password");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/test_stores", null, null);
    }
 
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String deleteTasksQuery = "DELETE FROM tasks *;";
-      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      String deleteTasksQuery = "DELETE FROM stores *;";
+      String deleteCategoriesQuery = "DELETE FROM brands *;";
       con.createQuery(deleteTasksQuery).executeUpdate();
       con.createQuery(deleteCategoriesQuery).executeUpdate();
     }
